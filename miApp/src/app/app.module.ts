c96fb71be443f1f33326092cd02511b58537145b
Eduardo Alanis <eduardoalanisrdz@hotmail.com>
2023-03-24 17:04:19 -0600

    Lista de Alumnos

--- miApp/src/app/app.module.ts
+++ miApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- miApp/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.css']
 })
-export class ListaAlumnosComponent {
+export class ListaAlumnosComponent implements OnInit{
+  
+  constructor() { }
+  ngOnInit():void {
+    
+  }
 
+
+  nombre = "Eduardo";
+  alumnos: any = ["Pablo", "Martin", "Brandon", "Orlando", "Ximena", "Andrea"];
+  
 }
+
+
+
