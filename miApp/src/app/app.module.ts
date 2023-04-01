49345f77a31e44cdfb7009efd1a7e730c159961d
G3sshoku <fidelmontalvo42@gmail.com>
2023-04-01 12:17:10 -0600

    Tienda de Productos

--- miApp/src/app/app.module.ts
+++ miApp/src/app/app.module.ts
@@ -5,15 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
 import { IonicModule } from '@ionic/angular';
+import { ProductosComponent } from './productos/productos.component';
+
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaAlumnosComponent
+    ListaAlumnosComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     IonicModule.forRoot()
   ],
   providers: [],
---
+++ miApp/src/app/productos/productos.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { ConsultasService } from '../consultas.service';
+
+@Component({
+  selector: 'app-productos',
+  templateUrl: './productos.component.html',
+  styleUrls: ['./productos.component.css']
+})
+export class ProductosComponent implements OnInit{
+
+  constructor(private consultas: ConsultasService) { };
+
+  productos: any = [ ];
+
+  categorias: any = [ ];
+
+  ngOnInit():void {
+    this.obtenerProductos();
+    this.obtenerCategorias();
+    this.filtrarProducto();
+    this.filtrarProductoCategorias();
+  }
+
+    obtenerProductos(){
+      this.consultas.getProductos().subscribe(res => {
+        this.productos = res;
+        console.log(res);
+      })
+    }
+   
+    obtenerCategorias(){
+      this.consultas.getCategorias().subscribe(res => {
+        console.log(res);
+      })
+    }
+
+    filtrarProducto(){
+      this.consultas.getProducto().subscribe(res => {
+        console.log(res);
+      })
+    }
+
+    filtrarProductoCategorias(){
+      this.consultas.getProductosPorCategoria(this.categoria).subscribe(res => {
+        console.log(res);
+      })
+    }
+
+    categoria : string ="";
+
+    filtrarProductoPorCategorias (ev:any){
+      console.log(ev);
+      this.categoria = ev.target.value;
+      console.log()
+
+    }
+}
