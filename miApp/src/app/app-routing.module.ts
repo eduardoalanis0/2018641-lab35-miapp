e09f00aac37d6824ba55573e51707559635c057c
lalo8115 <>
2023-04-22 10:42:01 -0500

    Por mientras

--- miApp/src/app/app-routing.module.ts
+++ miApp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [];
+path: 'inicio',
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- miApp/src/app/app.module.ts
+++ miApp/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
 import { IonicModule } from '@ionic/angular';
+import { ProductosComponent } from './productos/productos.component';
+import { BuscarComponent } from './buscar/buscar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaAlumnosComponent
+    ListaAlumnosComponent,
+    ProductosComponent,
+    BuscarComponent
   ],
   imports: [
     BrowserModule,
