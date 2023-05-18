cab495f98a56d1a42805234b326b64f7409c1ca1
eduardoalanis0 <eduardoalanisrdz@hotmail.com>
2023-05-18 00:51:40 -0600

    Galeria de fotos

--- miApp/miAppMovil/src/app/foto.service.ts
+++ miApp/miAppMovil/src/app/foto.service.ts
@@ -4,12 +4,19 @@ import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 
+import { Capacitor } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
+
 @Injectable({
   providedIn: 'root'
 })
 export class FotoService {
 
-  constructor() { }
+  constructor(
+    private platform: Platform
+  ) { }
+
+  public foto: UserFoto[] = [];
 
   public async addNewToGallery() {
     // Take a photo  
@@ -17,6 +24,10 @@ export class FotoService {
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100  });
+
+      const fotoGuardada = await this.savePhoto(capturedPhoto);
+  }
+  private async savePhoto(foto: Photo){
   }
 }
 
